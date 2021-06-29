1be9b1d83b5578bb64e435f57f3fd85066eeb923
danny zeng <danny.zeng@oocl.com>
2021-06-29 15:05:38 +0800

    introduce sender params to User

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -13,16 +13,15 @@ public class Parcel {
     private String senderAddress;
 
     public Parcel(String size, Double weight,
-        User receiver, String senderName, String senderPhoneNumber,
-        String senderAddress) {
+        User receiver, User sender) {
         this.size = size;
         this.weight = weight;
         this.receiverName = receiver.getBuyerName();
         this.receiverPhoneNumber = receiver.getBuyerPhoneNumber();
         this.receiverAddress = receiver.getBuyerAddress();
-        this.senderName = senderName;
-        this.senderPhoneNumber = senderPhoneNumber;
-        this.senderAddress = senderAddress;
+        this.senderName = sender.getBuyerName();
+        this.senderPhoneNumber = sender.getBuyerPhoneNumber();
+        this.senderAddress = sender.getBuyerAddress();
     }
 
     public String confirmReceiver(){
