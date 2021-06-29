09f12e60201051f2d27c9559f72d324008d95ad9
danny zeng <danny.zeng@oocl.com>
2021-06-29 14:57:20 +0800

    introduce params to User

---
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -0,0 +1,24 @@
+package com.tw.academy.basic.$5_data_clumps;
+
+public class Order {
+
+    private int orderNumber;
+
+    private String buyerName;
+    private String buyerPhoneNumber;
+    private String buyerAddress;
+
+    public Order(int orderNumber, User buyer) {
+        this.orderNumber = orderNumber;
+
+        this.buyerName = buyer.getBuyerName();
+        this.buyerPhoneNumber = buyer.getBuyerPhoneNumber();
+        this.buyerAddress = buyer.getBuyerAddress();
+    }
+
+    public String confirm(){
+        return String.format("Please confirm buyer information: buyer name is %s, " +
+                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+    }
+}
+
