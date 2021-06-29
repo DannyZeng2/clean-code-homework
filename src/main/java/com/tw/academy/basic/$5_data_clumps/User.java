8957063a50e2e8204435dc50d384d0fb4eb3b585
danny zeng <danny.zeng@oocl.com>
2021-06-29 15:16:20 +0800

    complete

--- src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -2,9 +2,9 @@ package com.tw.academy.basic.$5_data_clumps;
 
 public class User {
 
-    private final String buyerName;
-    private final String buyerPhoneNumber;
-    private final String buyerAddress;
+    private String buyerName;
+    private String buyerPhoneNumber;
+    private String buyerAddress;
 
     public User(String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.buyerName = buyerName;
